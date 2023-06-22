07d1033580e544e4e1c0516f1d2b6f2b6ae08933
sohel <mr.sohelsyed@gmail.com>
2023-06-23 00:14:46 +0530

    some changes in navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,12 @@
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <Navbar />
+      <h1 className="text-3xl font-bold underline h-48">Hello world!</h1>
+      <Footer />
+    </div>
+  );
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,60 @@
+import { useEffect, useState } from "react";
+
+// import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const navigation = [
+    { name: "Product", href: "#" },
+    { name: "Features", href: "#" },
+    { name: "Marketplace", href: "#" },
+    { name: "Company", href: "#" },
+  ];
+  useEffect(() => {
+    mobileMenuOpen;
+  }, []);
+  return (
+    <nav
+      className="flex items-center justify-between p-6 lg:px-8"
+      aria-label="Global"
+    >
+      <div className="flex lg:flex-1">
+        <a href="#" className="-m-1.5 p-1.5">
+          <span className="sr-only">Your Company</span>
+          <img
+            className="h-8 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt=""
+          />
+        </a>
+      </div>
+      <div className="flex lg:hidden">
+        <button
+          type="button"
+          className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          onClick={() => setMobileMenuOpen(true)}
+        >
+          <span className="sr-only">Open main menu</span>
+          <text className="h-6 w-6" aria-hidden="true" />
+        </button>
+      </div>
+      <div className="hidden lg:flex lg:gap-x-12">
+        {navigation.map((item) => (
+          <a
+            key={item.name}
+            href={item.href}
+            className="text-sm font-semibold leading-6 text-gray-900"
+          >
+            {item.name}
+          </a>
+        ))}
+      </div>
+      <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+        <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+          Log in <span aria-hidden="true">&rarr;</span>
+        </a>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
